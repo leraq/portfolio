727b2cbdc5ad43ba344e11f470a467ef393ab270
leraq <leraq@linux>
2021-08-11 15:32:07 +0100

    ci: add lint/type check to firebase action

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,51 +1,51 @@
-const path = require("path");
+const path = require('path')
 
 module.exports = {
   siteMetadata: {
-    title: "Leon Delaimy",
-    description: "Leon Delaimy: software engineer.",
-    siteUrl: "https://leondelaimy.com", // No trailing slash allowed!
-    image: "", // Path to your image you placed in the 'static' folder
+    title: 'Leon Delaimy',
+    description: 'Leon Delaimy: software engineer.',
+    siteUrl: 'https://leondelaimy.com', // No trailing slash allowed!
+    image: '', // Path to your image you placed in the 'static' folder
   },
   plugins: [
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-typescript",
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-typescript',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: "src/images/me.jpg",
+        icon: 'src/images/me.jpg',
       },
     },
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        name: 'pages',
+        path: './src/pages/',
       },
-      __key: "pages",
+      __key: 'pages',
     },
     {
-      resolve: "gatsby-plugin-alias-imports",
+      resolve: 'gatsby-plugin-alias-imports',
       options: {
         alias: {
-          "@components": path.resolve(__dirname, "src/components"),
-          "@images": path.resolve(__dirname, "src/images"),
-          "@pages": path.resolve(__dirname, "src/pages"),
+          '@components': path.resolve(__dirname, 'src/components'),
+          '@images': path.resolve(__dirname, 'src/images'),
+          '@pages': path.resolve(__dirname, 'src/pages'),
         },
       },
     },
   ],
-};
+}
